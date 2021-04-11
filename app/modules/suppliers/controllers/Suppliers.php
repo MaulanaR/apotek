e30550b21d903caf49441f3c131d9bf2a750664f
gawe007 <82104731+gawe007@users.noreply.github.com>
2021-04-12 01:17:53 +0700

    Tambah module
    Module : Supplier dan Unit.
    
    Belum dengan logic  apabila ada data yang menggunakan supplier/unit sehingga dicegah penghapusannya.

---
+++ app/modules/suppliers/controllers/Suppliers.php
@@ -0,0 +1,182 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * @author 		Maulana Rahman <maulana.code@gmail.com>
+*/
+class Suppliers extends CI_Controller {
+
+	private $privilege;
+
+	public function __construct()
+	{
+		parent::__construct();
+		//load model
+		$this->load->model('suppliers_model','model');
+
+		if(!$this->alus_auth->logged_in())
+		{
+			redirect('admin/Login','refresh');
+		}
+		$this->privilege = $this->Alus_hmvc->cek_privilege($this->uri->segment(1));
+        if($this->privilege['can_view'] == '0')
+        {
+            echo "<script type='text/javascript'>alert('You dont have permission to access this menu');</script>";
+            redirect('dashboard','refresh');
+        }
+	}
+		
+
+	public function index()
+	{
+	
+		if($this->alus_auth->logged_in())
+         {
+            $head['title'] = "Manajemen Supplier";
+         	//$data['tree'] = $this->model->all_tree();
+         	$data['can_add'] = $this->privilege['can_add'];
+    		
+		 	$this->load->view('template/temaalus/header',$head);
+		 	$this->load->view('suppliers/index.php',$data);
+		 	$this->load->view('template/temaalus/footer');
+		}else
+		{
+			redirect('admin/Login','refresh');
+		}
+	}
+
+	/* Server Side Data */
+	/* Modified by : Maulana.code@gmail.com */
+	public function ajax_list()
+    {
+        $list = $this->model->get_datatables();
+        $data = array();
+        $no = $_POST['start'];
+        foreach ($list as $record) {
+            $no++;
+            $row = array();
+            $row[] = $record->ms_nama;
+            $row[] = $record->ms_alamat;
+            $row[] = $record->ms_telp;
+            $row[] = $record->ms_kodepos;
+ 			if($this->privilege['can_edit'] == 1 && $this->privilege['can_delete'] == 1)
+            {
+                $row[] = '<a class="btn btn-sm btn-outline-primary" href="javascript:void(0)" title="Edit" onclick="edit_person('."'".$record->ms_id."'".')"><i class="glyphicon glyphicon-pencil"></i> Edit</a>
+                  <a class="btn btn-sm btn-outline-danger" href="javascript:void(0)" title="Hapus" onclick="delete_person('."'".$record->ms_id."'".')"><i class="glyphicon glyphicon-trash"></i> Delete</a>';
+            }
+
+            if($this->privilege['can_edit'] == 1 && $this->privilege['can_delete'] == 0)
+            {
+                $row[] = '<a class="btn btn-sm btn-outline-primary" href="javascript:void(0)" title="Edit" onclick="edit_person('."'".$record->ms_id."'".')"><i class="glyphicon glyphicon-pencil"></i> Edit</a>';
+            }
+
+            if($this->privilege['can_edit'] == 0 && $this->privilege['can_delete'] == 1)
+            {
+                $row[] = '<a class="btn btn-sm btn-outline-danger" href="javascript:void(0)" title="Hapus" onclick="delete_person('."'".$record->ms_id."'".')"><i class="glyphicon glyphicon-trash"></i> Delete</a>';
+            }
+
+            if($this->privilege['can_edit'] == 0 && $this->privilege['can_delete'] == 0)
+            {
+                $row[] = ' ';
+            }
+            //add html for action
+            $data[] = $row;
+        }
+ 
+        $output = array(
+                        "draw" => $_POST['draw'],
+                        "recordsTotal" => $this->model->count_all(),
+                        "recordsFiltered" => $this->model->count_filtered(),
+                        "data" => $data,
+                );
+        //output to json format
+        echo json_encode($output);
+    }
+
+    public function ajax_edit($id)
+    {
+        $data = $this->model->get_by_id($id);
+        $ms_nama = $data->ms_nama;
+        $arr = array('data' => $data,
+                     'nama' => $ms_nama,
+                     );
+        echo json_encode($arr);
+    }
+ 
+    public function ajax_add()
+    {
+    	if($this->privilege['can_add'] == 0)
+		{
+			echo json_encode(array("status" => FALSE,"msg" => "You Dont Have Permission"));
+		}
+        else
+        {
+
+        $this->form_validation->set_rules('name', 'Nama Supplier', 'required');
+        $this->form_validation->set_rules('alamat', 'Alamat', 'required');
+        $this->form_validation->set_rules('telp', 'Nomor Telp', 'required');
+        $this->form_validation->set_rules('kodepos', 'Kode Pos', 'required');
+        //$this->form_validation->set_rules('uri', 'URI', 'required');
+        //$this->form_validation->set_rules('parent', 'Parent Menu', 'required');
+
+        if ($this->form_validation->run() == true)
+        {
+            $data = array(
+                'ms_nama' => $this->input->post('name'),
+                'ms_alamat' => $this->input->post('alamat'),
+                'ms_telp' => $this->input->post('telp'),
+                'ms_kodepos' => $this->input->post('kodepos'),
+            );
+            $insert = $this->model->save($data);
+            echo json_encode(array("status" => TRUE));
+        }else{
+            echo json_encode(array("status" => FALSE,"msg" => validation_errors() ));
+        }
+        }
+        
+    }
+ 
+    public function ajax_update()
+    {
+    	if($this->privilege['can_edit'] == 0)
+		{
+			echo json_encode(array("status" => FALSE,"msg" => "You Dont Have Permission"));
+		}else{
+
+        $this->form_validation->set_rules('name', 'Nama Supplier', 'required');
+        $this->form_validation->set_rules('alamat', 'Alamat', 'required');
+        $this->form_validation->set_rules('telp', 'Nomor Telp', 'required');
+        $this->form_validation->set_rules('kodepos', 'Kode Pos', 'required');
+
+        if ($this->form_validation->run() == true)
+        {
+            $data = array(
+                'ms_nama' => $this->input->post('name'),
+                'ms_alamat' => $this->input->post('alamat'),
+                'ms_telp' => $this->input->post('telp'),
+                'ms_kodepos' => $this->input->post('kodepos'),
+            );
+            $this->model->update(array('ms_id' => $this->input->post('id')), $data);
+            echo json_encode(array("status" => TRUE));
+        }else{
+            echo json_encode(array("status" => FALSE,"msg" => validation_errors()));
+        }
+        }
+        
+    }
+ 
+    public function ajax_delete()
+    {
+    	if($this->privilege['can_delete'] == 0)
+		{
+			echo json_encode(array("status" => FALSE,"msg" => "You Dont Have Permission"));
+		}else
+        {
+            $this->model->delete_by_id($this->input->post('id'));
+            //$this->model->delete_detail_grupakses($this->input->post('id'));
+            echo json_encode(array("status" => TRUE, "id" => $this->input->post('id')));
+        }
+    }
+}
+
+/* End of file  Home.php */
+/* Location: ./application/controllers/ Home.php */
---
+++ app/modules/unit/Views/index.php
@@ -0,0 +1,254 @@
+  <!-- Full Width Column -->
+  <div class="content-wrapper" style="min-height: 901px;">
+    <div class="container-fluid">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          Manajemen Unit
+        </h1>
+      </section>
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="callout callout-info">
+          <p>Lorem ipsum dolor .</p>
+        </div>
+          <div class="card">
+            <div class="card-header text-right">
+              <div class="btn-group">
+                  <?php if($can_add == 1){?>
+                  <button class="btn btn-sm btn-success" onclick="add_person()"><i class="fas fa-plus"></i> Tambah</button>
+                  <?php } ?>
+                  <button class="btn btn-sm btn-default" onclick="reload_table()"><i class="fas fa-retweet"></i> Reload</button>
+              </div>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <div class="dataTables_wrapper dt-bootstrap4">
+                <div class="row">
+                  <div class="col-12">
+                    <table id="table" class="table table-bordered table-hover dataTable dtr-inline" style="width: 100%">
+                      <thead>
+                      <tr>
+                        <th>Nama Unit</th>
+                        <th>Tools</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.container -->
+  </div>
+  <!-- /.content-wrapper -->
+<!--Modal -->
+<div class="modal" id="modal_form">
+          <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h4 class="modal-title">Tambah Menu</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">×</span></button>
+              </div>
+              <div class="modal-body">
+                <form action="#" id="form" class="form-horizontal" name="formnih">
+                    <input type="hidden" value="" name="id"/> 
+                    <div class="form-body">
+                        <div class="form-group">
+                            <label class="control-label ">Nama Unit</label>
+                            
+                                <input type="text" name="name" class="form-control" placeholder="Nama">
+                                <span class="help-block"></span>
+                            
+                        </div>
+                    </div>
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" id="btnSave" onclick="save()" >Save</button>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+  <script type="text/javascript">
+ 
+var save_method; //for save method string
+var table;
+ 
+$(document).ready(function() {
+
+    table = $('#table').DataTable({ 
+        "processing": true, //Feature control the processing indicator.
+        "serverSide": true, //Feature control DataTables' server-side processing mode.
+        scroller:true,
+        scrollX : true,
+        // Load data for the table's content from an Ajax source
+        "ajax": {
+            "url": "<?php echo site_url('unit/ajax_list')?>",
+            "type": "POST"
+        },
+ 
+        //Set column definition initialisation properties.
+        "columnDefs": [
+        { 
+            "targets": [ -1 ], //last column
+            "orderable": false, //set not orderable
+            "className":"text-center",
+        },
+        { 
+            "targets": [1], //last column
+            "className":"text-center",
+        },
+        ],
+    });
+
+});
+ 
+function add_person()
+{
+    save_method = 'add';
+    $('#form')[0].reset(); // reset form on modals
+    $('.form-group').removeClass('has-error'); // clear error class
+    $('.help-block').empty(); // clear error string
+    $('#modal_form').modal('show'); // show bootstrap modal
+    $('.modal-title').text('Add Menu'); // Set Title to Bootstrap modal title
+}
+ 
+function edit_person(id)
+{
+    save_method = 'update';
+    $('#form')[0].reset(); // reset form on modals
+    $('.form-group').removeClass('has-error'); // clear error class
+    $('.help-block').empty(); // clear error string
+ 
+    //Ajax Load data from ajax
+    $.ajax({
+        url : "<?php echo site_url('unit/ajax_edit/')?>/" + id,
+        type: "GET",
+        dataType: "JSON",
+        success: function(data)
+        {
+ 
+            $('[name="id"]').val(data.data.mu_id);
+            $('[name="name"]').val(data.nama);
+            //document.formnih.parent.value=data.data.menu_parent;
+            //document.formnih.icon.value=data.data.menu_icon;
+
+            $('#modal_form').modal('show'); // show bootstrap modal when complete loaded
+            $('.modal-title').text('Edit Unit'); // Set title to Bootstrap modal title
+ 
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        {
+            alert('Error get data from ajax');
+        }
+    });
+}
+ 
+function reload_table()
+{
+    table.ajax.reload(null,false); //reload datatable ajax 
+    //refresh_menu_list();
+}
+
+/*function refresh_menu_list() {
+     $.ajax({
+          type:"GET",
+          url: "<?php //echo base_url('unit/refresh_menu_list/');?>",
+          dataType:"JSON",
+          beforeSend: function() 
+          { 
+           },
+          success: function(json){
+            $('#treenih').append('<tr><td class="text-right"><input type="radio" class="radio" name="parent" value="0" checked></td><td>Ini Parent Menu</td></tr>');
+            $.each(json['data'], function(j, value) {
+                  $('#treenih').append('<tr><td class="text-right"><input type="radio" class="radio" name="parent" value="'+value.menu_id+'"></td><td>'+value.menu_nama+'</td></tr>');
+                });
+          }
+      });
+}*/
+ 
+function save()
+{
+    $('#btnSave').text('saving...'); //change button text
+    $('#btnSave').attr('disabled',true); //set button disable 
+    var url;
+ 
+    if(save_method == 'add') {
+        url = "<?php echo site_url('unit/ajax_add')?>";
+    } else {
+        url = "<?php echo site_url('unit/ajax_update')?>";
+    }
+ 
+    // ajax adding data to database
+    $.ajax({
+        url : url,
+        type: "POST",
+        data: $('#form').serialize(),
+        dataType: "JSON",
+        success: function(data)
+        {
+ 
+            if(data.status) //if success close modal and reload ajax table
+            {
+                popup('Informasi','Data berhasil simpan');
+                $('#modal_form').modal('hide');
+                reload_table();
+                //$("#treenih").empty();
+                //refresh_menu_list();
+            }else{
+                popup('Perhatian',data.msg,'info');
+                reload_table();
+                //$("#treenih").empty();
+                //refresh_menu_list();
+            }
+ 
+            $('#btnSave').text('save'); //change button text
+            $('#btnSave').attr('disabled',false); //set button enable 
+        }
+    });
+}
+ 
+function delete_person(id)
+{
+    if(confirm('Are you sure delete this data?'))
+    {
+        // ajax delete data to database
+        $.ajax({
+            url : "<?php echo site_url('unit/ajax_delete')?>/",
+            type: "POST",
+            data:{'id' : id},
+            dataType: "JSON",
+            success: function(data)
+            {
+               if(data.status) //if success close modal and reload ajax table
+              {
+                  $('#modal_form').modal('hide');
+                  popup('Informasi','Data berhasil dihapus');
+                  reload_table();
+              }else{
+                  popup('Perhatian',data.msg,'info');
+                  reload_table();
+              }
+            }
+        });
+ 
+    }
+}
+
+
+</script>
